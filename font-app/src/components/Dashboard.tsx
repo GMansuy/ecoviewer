7d35e8c7c59278a18b8659c31ae1288032f1ccac
Gabriel Mansuy <gabriel.mansuy@gmail.com>
2024-03-13 13:54:07 +0100

    c

---
+++ font-app/src/components/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React, {useEffect} from 'react'
+
+const Dashboard = () => {
+
+    useEffect(() => {
+        
+    })
+
+    return (
+        <div>Des choses vont s'afficher ici</div>
+    )
+
+} 
+
+export default Dashboard
